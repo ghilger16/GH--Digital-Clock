63fee9d511bc4dbbc5038cc97bbafd0786ced7b2
ghilger16 <ghilger16@yahoo.com>
2020-12-08 15:39:07 -0700

    day 2 first commit

--- script.js
+++ script.js
@@ -4,31 +4,38 @@ const clockEL = document.querySelector(".clock");
 const messageEL = document.querySelector(".message");
 
 setInterval(function () {
-const now = new Date();
-const year = now.getFullYear();
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-const month = months[now.getMonth()];
-const weekdayList = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-const day = weekdayList[now.getDay()];
-const time = now.toLocaleTimeString();
+  const now = new Date();
+  const year = now.getFullYear();
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const month = months[now.getMonth()];
+  const weekdayList = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const weekday = weekdayList[now.getDay()];
+  const day = now.getDate();
+  const time = now.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+  messageEL.textContent = `${time} ${weekday}, ${month} ${day} ${year}`;
+}, 0);
